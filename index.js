05912a344bce1b8f78926cad83e358d0c7030ca0
Jessica Schmidt <129232792+JessicaSchmidtG@users.noreply.github.com>
2023-04-12 22:01:50 -0300

    Aula e exercícios Operadores

---
+++ index.js
@@ -0,0 +1,41 @@
+//Graus Fahrenheit  p/ kelvin
+//(KELVIN) = (GRAUS_FAHRENHEIT -32)*(5/9) + 273.15
+
+//GRAUS CELSIUS P/ FAHRENHEIT
+//(GRAUS_FAHRENHEIT) = (GRAUS_CELSIUS)*(9/5) + 32
+
+//EXERCICIO 1
+/*let fahrenheit1 = 77
+kelvin1 = (fahrenheit1 - 32)*(5/9) + 273.15
+console.log("77 graus Fahrenheit em Kelvin é: ", kelvin1)
+
+//EXERCÍCIO 2
+let celsius2 = 80
+fahrenheit2 = (celsius2)*(9/5) + 32
+console.log("80 graus Celsius em Fahrenheit é:",fahrenheit2)
+
+//EXERCÍCIO 3
+let celsius3 = 30
+fahrenheit3 = (celsius3)*(9/5) + 32
+console.log("30 graus Celsius em Fahrenheit é: ",fahrenheit3)
+kelvin3 = (fahrenheit3 - 32)*(5/9) + 273.15
+console.log("30 graus Celsius em Kelvin é: '", kelvin3)
+
+//EXERCÍCIO 4
+let celsius4 = parseFloat(prompt("Digite o valor em graus Celsius que deseja converter:"))
+fahrenheit4 = (celsius4)*(9/5) + 32
+console.log(celsius4+"graus Celsius em Fahrenheit é: ", fahrenheit4)
+kelvin4 = (fahrenheit4 - 32)*(5/9) + 273.15
+console.log(celsius4+"graus Celsius em Kelvin é:",kelvin4)
+*/
+//EXERCÍCIO 5
+let qW_h = 0.05
+let consumo = parseFloat(prompt("Digite a quantidade de quilowatts consumida na residência: "))
+//A) Calcule e mostre o valor a ser pago por uma residencia que consuma 280 quilowatt-hora
+let valor = consumo*0.05
+console.log("Ovalor a ser pago pelo consumo de 280 quilowatts-hora é de R$: "+valor, "reais.")
+
+/*B) Altere o programa para receber mais um valor: a porcentagem de desconto.
+Calcule e mostre o valor a ser pago pela mesma residencia acima 
+considerando 15% de desconto
+*/
